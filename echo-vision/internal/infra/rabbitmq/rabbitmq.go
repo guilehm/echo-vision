67e1629277e66ba304b478a48ab83fb1564b7324
gui <guihm.dev@gmail.com>
2025-01-12 23:01:59 -0300

    refactor rabbitmq

--- echo-vision/internal/infra/rabbitmq/rabbitmq.go
+++ echo-vision/internal/infra/rabbitmq/rabbitmq.go
@@ -1,8 +1,10 @@
 package rabbitmq
 
 import (
-	"fmt"
+	"errors"
+	"os"
 
+	"github.com/guilehm/echo-vision/internal/app/ports"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -19,36 +21,31 @@ const (
 	ExchangeTypeHeaders ExchangeType = "headers"
 )
 
-type RabbitMQClient struct {
+type RabbitMQAdapter struct {
 	connection *amqp.Connection
-	channel    *amqp.Channel
 }
 
-func ConnectRabbitMQ(username, password, host, vhost string) (*amqp.Connection, error) {
-	return amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s/%s", username, password, host, vhost))
-}
+func NewRabbitMQAdapter() (ports.AsyncMessagingPort, error) {
+	rabbitURL := os.Getenv("RABBITMQ_URL")
+	if rabbitURL == "" {
+		return nil, errors.New("RABBITMQ_URL is required")
+	}
 
-func NewRabbitMQClient(conn *amqp.Connection) (*RabbitMQClient, error) {
-	ch, err := conn.Channel()
+	conn, err := amqp.Dial(rabbitURL)
 	if err != nil {
 		return nil, err
 	}
-	return &RabbitMQClient{connection: conn, channel: ch}, nil
+	return &RabbitMQAdapter{connection: conn}, nil
 }
 
-func (rc *RabbitMQClient) Close() error {
-	return rc.channel.Close()
+func (r *RabbitMQAdapter) CreateChannel() (ports.MessagingChannel, error) {
+	ch, err := r.connection.Channel()
+	if err != nil {
+		return nil, err
+	}
+	return &RabbitMQChannel{channel: ch}, nil
 }
 
-func (rc *RabbitMQClient) CreateExchange(name ExchangeName, exchangeType ExchangeType, durable bool) error {
-	return rc.channel.ExchangeDeclare(name.String(), exchangeType.String(), durable, false, false, false, nil)
-}
-
-func (rc *RabbitMQClient) CreateQueue(name QueueName, durable, autodelete bool) error {
-	_, err := rc.channel.QueueDeclare(name.String(), durable, autodelete, false, false, nil)
-	return err
-}
-
-func (rc *RabbitMQClient) CreateBinding(name QueueName, key string, exchange ExchangeName) error {
-	return rc.channel.QueueBind(name.String(), key, exchange.String(), false, nil)
+func (r *RabbitMQAdapter) Close() error {
+	return r.connection.Close()
 }
