40e868796f647348f8656c569e1be03309d199ca
gui <guihm.dev@gmail.com>
2025-01-13 20:09:48 -0300

    create queue

--- echo-vision/internal/infra/rabbitmq/queues.go
+++ echo-vision/internal/infra/rabbitmq/queues.go
@@ -5,7 +5,3 @@ type QueueName string
 func (q QueueName) String() string {
 	return string(q)
 }
-
-const (
-	QueueNameCustomersCreated QueueName = "customers_created"
-)
--- echo-vision/main.go
+++ echo-vision/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/guilehm/echo-vision/internal/infra/rabbitmq"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	conn, err := rabbitmq.NewRabbitMQAdapter()
+	if err != nil {
+		log.Fatalln("could not create rabbitmq adapter: ", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.CreateChannel()
+	if err != nil {
+		log.Fatalln("could not create channel: ", err)
+	}
+
+	fmt.Println("channel", ch)
 }
