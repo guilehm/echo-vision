edb22f485124f16bc52a804442c433053a3f773c
gui <guihm.dev@gmail.com>
2025-01-13 22:22:53 -0300

    refactor ports and infra

--- echo-vision/internal/app/ports/imageRecognitionService.go
+++ echo-vision/internal/app/ports/imageRecognitionService.go
@@ -2,8 +2,8 @@ package ports
 
 import "github.com/guilehm/echo-vision/internal/app/domain"
 
-// ImageRecognitionPort defines the behavior required for image recognition.
-type ImageRecognitionPort interface {
+// ImageRecognitionServicePort defines the interface for image recognition services.
+type ImageRecognitionServicePort interface {
 	DetectLabels(imageBytes []byte) ([]domain.Label, error)
 	DetectFaces(imageBytes []byte) ([]domain.FaceDetail, error)
 }
