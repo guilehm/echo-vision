fbbc1c087ba0a704c7cc6a96caedaf4ab58d56d3
gui <guihm.dev@gmail.com>
2025-01-14 21:35:51 -0300

    create errors

---
+++ echo-vision/internal/app/shared/errors.go
@@ -0,0 +1,10 @@
+package shared
+
+import "errors"
+
+var (
+	ErrInvalidID        = errors.New("invalid id")
+	ErrInvalidStatus    = errors.New("invalid status")
+	ErrInvalidEventType = errors.New("invalid event type")
+	ErrInvalidPayload   = errors.New("invalid payload")
+)
