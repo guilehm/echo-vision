fcaed15472a7d7d7b40903add152a939c8f979f7
gui <guihm.dev@gmail.com>
2025-01-14 23:07:00 -0300

    create user repository

--- echo-vision/internal/app/repositories/userRepository.go
+++ echo-vision/internal/app/repositories/userRepository.go
@@ -1,1 +1,12 @@
 package repositories
+
+import (
+	"context"
+
+	"github.com/guilehm/echo-vision/internal/app/domain"
+)
+
+type UserRepository interface {
+	Save(ctx context.Context, tx Transaction, event *domain.User) error
+	FindByEmail(ctx context.Context, email string) (*domain.User, error)
+}
