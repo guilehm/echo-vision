cdce6483bb113bebd05c53ff7f1c32e9cac09d61
gui <guihm.dev@gmail.com>
2025-04-20 19:57:34 -0300

    use the base url from the container

--- echo-ui/src/services/server-requester.js
+++ echo-ui/src/services/server-requester.js
@@ -3,7 +3,7 @@
 import { cookies } from "next/headers";
 
 // TODO: move to an environment variable
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "http://echo-hub";
 
 export async function getPresignedUrl({ filename, eventType, contentType }) {
   const c = await cookies();
