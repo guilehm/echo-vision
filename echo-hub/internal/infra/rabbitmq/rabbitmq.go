731ece0ae982bf230d1408e1c7b1cf50a5c81304
gui <guihm.dev@gmail.com>
2025-06-06 20:04:14 -0300

    register correct events

--- echo-hub/internal/infra/rabbitmq/rabbitmq.go
+++ echo-hub/internal/infra/rabbitmq/rabbitmq.go
@@ -2,27 +2,46 @@ package rabbitmqadapter
 
 import (
 	"context"
+	"encoding/json"
+	"log/slog"
 
+	"github.com/guilehm/echo-vision/echo-common/logging"
 	"github.com/guilehm/echo-vision/echo-common/pkg/messaging"
 	hubevents "github.com/guilehm/echo-vision/echo-hub/pkg/events"
 )
 
-type RabbitMQAdapter struct{}
+var logger = logging.NewLogger()
+
+type RabbitMQAdapter struct {
+	// consumer  ports.ConsumerPort
+}
 
 func (r *RabbitMQAdapter) Topics() []string {
 	return []string{
-		hubevents.EventImageAnalysCreated,
 		hubevents.EventImageAnalysisStatusUpdated,
 	}
 }
 
 func (r *RabbitMQAdapter) Handle(ctx context.Context, msg messaging.Message) messaging.HandlerResponse {
 	switch msg.Topic {
-	case hubevents.EventImageAnalysCreated:
+	case
+		hubevents.EventImageAnalysisStatusUpdatedCompleted,
+		hubevents.EventImageAnalysisStatusUpdatedFailed,
+		hubevents.EventImageAnalysisStatusUpdatedProcessing,
+		hubevents.EventImageAnalysisStatusUpdatedPending:
+		var message hubevents.EventStatusUpdateMessage
+		if err := json.Unmarshal(msg.Payload, &message); err != nil {
+			logger.Error(
+				"could not unmarshal event",
+				slog.String("error", err.Error()),
+			)
+			return messaging.DeadLetter
+		}
+		logger.Info("received image analysis status update",
+			slog.String("id", message.ID.String()),
+			slog.String("status", string(message.Status)))
+		// return r.consumer.ProcessImageAnalysis(msg.Topic, message)
 		return messaging.Success
-	// case hubevents.EventImageAnalysisStatusUpdated:
-	// 	fmt.Println("Image analysis status updated")
-	// 	return messaging.Success
 	default:
 		return messaging.DeadLetter
 	}
