b44f08fc72f00d509ce510ffb77df9fa064d7336
gui <guihm.dev@gmail.com>
2025-06-07 10:41:12 -0300

    update publisher for image analysis

--- echo-analyzer/internal/infra/publishers/image_analysis.go
+++ echo-analyzer/internal/infra/publishers/image_analysis.go
@@ -2,22 +2,33 @@ package publishers
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/google/uuid"
+	analyzerevents "github.com/guilehm/echo-vision/echo-analyzer/pkg/events"
 	"github.com/guilehm/echo-vision/echo-common/pkg/messaging"
-	hubevents "github.com/guilehm/echo-vision/echo-hub/pkg/events"
 	"github.com/rotisserie/eris"
 )
 
 // PublishImageAnalysisStatusUpdate implements ports.PublisherPort.
-func (p *PublisherGroup) PublishImageAnalysisStatusUpdate(ctx context.Context, eventID uuid.UUID, message hubevents.EventStatusUpdateMessage) error {
-	payload, err := message.ToJSON()
-	if err != nil {
-		return eris.Wrap(err, "failed to convert message to JSON")
+func (p *PublisherGroup) PublishImageAnalysisStatusUpdate(
+	ctx context.Context,
+	eventID uuid.UUID,
+	status analyzerevents.EventStatus,
+	data json.RawMessage,
+) error {
+	message := analyzerevents.EventImageAnalysisStatusUpdateMessage{
+		ID:     eventID,
+		Status: status,
+		Data:   data,
 	}
 
+	payload, err := json.Marshal(message)
+	if err != nil {
+		return eris.Wrap(err, "failed to marshal event message")
+	}
 	err = p.publisher.Publish(ctx, messaging.Message{
-		Topic:   hubevents.BuildEventStatusUpdatedTopic(message.Type, message.Status),
+		Topic:   analyzerevents.BuildEventImageAnalysisStatusUpdatedTopic(status),
 		Payload: payload,
 	})
 	if err != nil {
