043c07f899dcac1f2266f4b2d384119d430852bd
gui <guihm.dev@gmail.com>
2025-06-10 20:29:28 -0300

    workaround for now

--- echo-ui/src/utils.js
+++ echo-ui/src/utils.js
@@ -2,9 +2,10 @@ export const formatDate = (dateString) => {
   const options = {
     year: "numeric",
     month: "short",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
+    // TODO: Uncomment the following lines to include day, hour, and minute
+    // day: "numeric",
+    // hour: "2-digit",
+    // minute: "2-digit",
   };
   return new Date(dateString).toLocaleDateString("en-US", options);
 };
