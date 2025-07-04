216e571d9bf1e100b28e7c58881c55c2ce152991
gui <guihm.dev@gmail.com>
2025-07-04 14:42:59 -0300

    handle unauthorized

--- echo-ui/src/app/dashboard/analysis/images/[uuid]/page.jsx
+++ echo-ui/src/app/dashboard/analysis/images/[uuid]/page.jsx
@@ -3,6 +3,7 @@ import ImageAnalysisDetail from "@/components/analysis/imageDetails";
 import { getEventById } from "@/services/client";
 import { serverRequester } from "@/services/server-requester";
 import { notFound } from "next/navigation";
+import { redirect } from "next/navigation";
 
 const headerProps = {
   breadcrumbData: [
@@ -16,13 +17,19 @@ const headerProps = {
 export default async function ImageAnalysisDetailPage({ params }) {
   const { uuid } = await params;
   const response = await getEventById(serverRequester, uuid);
-  if (!response || response.status !== 200) {
+
+  if (!response || response.status === 404) {
     notFound();
   }
 
   const data = response.data;
   if (!data) {
     notFound();
+  }
+
+  if (response.status === 403 || response.status === 401) {
+    // TODO: handle unauthorized access
+    redirect("/sign-in");
   }
 
   return (
