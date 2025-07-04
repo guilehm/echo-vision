e89d11f69e8709819e532d9b9bd9209b43a6ec09
gui <guihm.dev@gmail.com>
2025-07-04 19:05:50 -0300

    set tokens lifetime

--- echo-hub/internal/app/ports/tokens.go
+++ echo-hub/internal/app/ports/tokens.go
@@ -1,8 +1,18 @@
 package ports
 
-import "github.com/guilehm/echo-vision/echo-hub/internal/app/domain"
+import (
+	"time"
+
+	"github.com/guilehm/echo-vision/echo-hub/internal/app/domain"
+)
 
 type TokenClaims map[string]any
+
+const (
+	// AccessTokenLifetime  = 1 * time.Minute
+	AccessTokenLifetime  = 15 * time.Minute
+	RefreshTokenLifetime = 1 * 24 * time.Hour
+)
 
 type TokenManager interface {
 	GenerateAccessToken(user *domain.User) (string, error)
