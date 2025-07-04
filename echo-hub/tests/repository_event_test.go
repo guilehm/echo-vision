83b8dd61b574c7198fe86248dbe73221d4975a16
gui <guihm.dev@gmail.com>
2025-07-04 14:01:32 -0300

    fix test

--- echo-hub/tests/repository_event_test.go
+++ echo-hub/tests/repository_event_test.go
@@ -31,18 +31,19 @@ var _ = Describe("Event Repository", func() {
 
 	It("SaveEvent with file", func() {
 		// Arrange
-		eventID, err := repo.SaveEvent(ctx, nil, validEventWithFile(validUser))
+		ve := validEventWithFile(validUser)
+		eventID, err := repo.SaveEvent(ctx, nil, ve)
 		Expect(err).ToNot(HaveOccurred())
 
 		// Assert
 		e, err := repo.FindEventByID(ctx, nil, eventID)
 		Expect(err).ToNot(HaveOccurred())
 
-		Expect(e.ID().String()).To(Equal(validEventWithFile(validUser).ID().String()))
+		Expect(e.ID().String()).To(Equal(ve.ID().String()))
 		Expect(e.File).ToNot(BeNil())
-		Expect(e.File().Filename).To(Equal(validEventWithFile(validUser).File().Filename))
-		Expect(e.File().Filepath).To(Equal(validEventWithFile(validUser).File().Filepath))
-		Expect(e.File().Filesize).To(Equal(validEventWithFile(validUser).File().Filesize))
-		Expect(e.File().ContentType).To(Equal(validEventWithFile(validUser).File().ContentType))
+		Expect(e.File().Filename).To(Equal(ve.File().Filename))
+		Expect(e.File().Filepath).To(Equal(ve.File().Filepath))
+		Expect(e.File().Filesize).To(Equal(ve.File().Filesize))
+		Expect(e.File().ContentType).To(Equal(ve.File().ContentType))
 	})
 })
